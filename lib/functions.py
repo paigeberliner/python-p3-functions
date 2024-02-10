0042ce079415716d1c77f216735ab0cd75f625ec
Paige <paigeberliner@gmail.com>
2024-02-10 09:48:58 -0500

    Complete Lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python3
-
 def greet_programmer():
-    pass
+   print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+greet_with_default("Sunny")
 
 def add(num1, num2):
-    pass
+    sum_result = num1 + num2
+    return sum_result
+
+result = add(1, 2)
+print(result)
 
 def halve(number):
-    pass
+    if type(number) in (int, float):
+        return number / 2
+    else:
+        return None
+
+result = halve(4)
+print(result)
+
+result = halve("two")
+print(result)
+
